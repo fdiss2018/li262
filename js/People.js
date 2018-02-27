001dc76dd1e2eba639a6615216a850e3b7ef3372
fdiss2018 <dissoubray@yahoo.fr>
2018-02-27 17:09:26 +0100

    27/02 endday

--- js/People.js
+++ js/People.js
@@ -1,13 +1,13 @@
-class People{
-    constructor (id, nom, prenom, age = 18) {
+class People {
+    constructor(id, nom, prenom, age = 18) {
         this.nom = nom;
         this.prenom = prenom;
         this.age = age;
-        this.id = Math.floor(Math.random())*1000;
+        this.id = Math.floor(Math.random()) * 1000;
         this.id = id;
     }
 
-    getNomComplet(){
+    getNomComplet() {
         return this.nom + " " + this.prenom;
     }
 
