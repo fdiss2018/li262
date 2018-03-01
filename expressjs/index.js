f29ff731dbfdc59803da262c93c5e260d15a2089
fdiss2018 <dissoubray@yahoo.fr>
2018-03-01 17:30:42 +0100

    endday 01/03

--- expressjs/index.js
+++ expressjs/index.js
@@ -5,6 +5,11 @@ const app = express();
 
 const users = require("./routes/userRoutes.js");
 const articlesR = require("./routes/articleRoutes.js");
+const loginR = require("./routes/login.js");
+
+// const loginR = require("./routes/login.js");
+
+// NB dans package.json ce fichier est déclaré en tant que main. c'est donc le point d'entré de l'appli.
 
 // Demnde à express de parser le body de la requete http. Permet de récuperer les paramêtres du formulaire (cf articles en post)
 const bodyParser = require('body-parser');
@@ -14,7 +19,7 @@ app.use(bodyParser.json());
 // permet d'indiquer que la requete s'appuie sur le repertoire static pour afficher une ressource (coté client).
 app.use(express.static(path.join(__dirname, 'static')));
 
-console.log("Ma requete");
+console.log("Ma requete : ");
 
 ////////////////////////////////////////////////////////////////////////////////////////
 //////////////      Mes routes                            //////////////////////////////
@@ -66,16 +71,29 @@ app.get("/hello",function(req, res){
 
 // Pour aller plus loin
 app.get("/form/form.html",function(req, res){
+    // On passe un JSOn à la vue pour affichage
     const title = "Création d'article";
+    const produits = [];
+    const p1 = {"titre" : "McBook", "prix":22};produits.push(p1);
+    const p2 = {"titre" : "Iphone", "prix":12};produits.push(p2);
+    const p3 = {"titre" : "Nokia3100" };produits.push(p3);
     res.render("article", {
-        "myTitle": title
+        "myTitle": title,
+        "inventaire" : produits
     });
 });
 
 ///////////////////////////////////////////////////////////////////////////////////
-// Nestx
+// exercice login
 ///////////////////////////////////////////////////////////////////////////////////
 
+app.get("/loginF",function(req, res){
+    // On passe un JSOn à la vue pour affichage
+    res.render("login", {});
+});
+
+// Methode plus propre : On associe une fonction à une route
+app.use("/login",loginR);
 
 const server = http.createServer(app);
 server.listen(3001);
